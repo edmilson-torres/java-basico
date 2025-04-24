882996f359394e7ebfa389c3c83fdb17bd20bca8
Edmilson Torres <edmilson.torres@gmail.com>
2025-04-23 22:07:26 -0300

    feat: add Main class to demonstrate iPhone functionalities

---
+++ interfaces/src/Main.java
@@ -0,0 +1,11 @@
+import iPhone.iPhone;
+
+public class Main {
+    public static void main(String[] args) {
+        iPhone iphone = new iPhone();
+        iphone.selecionarMusica("Música 1");
+        iphone.tocar();
+        iphone.ligar("123456789");
+        iphone.exibirPagina("www.example.com");
+    }
+}
